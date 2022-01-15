289475b90e7afbebf6e781eeb74bdc847826a51c
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2022-01-15 13:55:19 -0500

    💄  Add header styles for desktop screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
       },
       backgroundImage: {
         bgHeroMobile: "url(/images/bg-hero-mobile.svg)",
+        bgHeroDesktop: "url(/images/bg-hero-desktop.svg)",
       },
     },
   },
