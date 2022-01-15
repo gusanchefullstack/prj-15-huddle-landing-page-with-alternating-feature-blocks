48b0de7ac5536ac28e0129e585dd9916a9fb7eb8
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2022-01-14 19:37:38 -0500

    📱  Add styles for mobile screen (375px)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
-  content: [],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        openSans: ["Open Sans", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        pinky: "hsl(322, 100%, 66%)",
+        veryPaleCyan: "hsl(193, 100%, 96%)",
+        veryDarkCyan: "hsl(192, 100%, 9%)",
+        grayishBlue: "hsl(208, 11%, 55%)",
+      },
+      backgroundImage: {
+        bgHeroMobile: "url(/images/bg-hero-mobile.svg)",
+      },
+    },
   },
   plugins: [],
-}
+};
